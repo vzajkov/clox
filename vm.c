66f22a592f634c6f91f44602393742ef3736684b
Valery Zajkov <vzajkov@protonmail.com>
2023-04-24 18:08:33 -0400

    15.2 Value stack

--- vm.c
+++ vm.c
@@ -6,9 +6,25 @@
 
 VM vm;
 
-void initVM() {}
+static void resetStack() {
+  vm.stackTop = vm.stack;
+}
+
+void initVM() {
+  resetStack();
+}
 
 void freeVM() {}
+
+void push(Value value) {
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+
+Value pop() {
+  vm.stackTop--;
+  return *vm.stackTop;
+}
 
 static InterpretResult run() {
   #define READ_BYTE() (*vm.ip++)
@@ -16,6 +32,13 @@ static InterpretResult run() {
 
   for(;;) {
     #ifdef DEBUG_TRACE_EXECUTION
+    printf("          ");
+    for(Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+      printf("[ ");
+      printValue(*slot);
+      printf(" ]");
+    }
+    printf("\n");
     disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
     #endif
 
@@ -23,11 +46,14 @@ static InterpretResult run() {
     switch (instruction = READ_BYTE()) {
       case OP_CONSTANT: {
         Value constant = READ_CONSTANT();
+        push(constant);
         printValue(constant);
         printf("\n");
         break;
       }
       case OP_RETURN: {
+        printValue(pop());
+        printf("\n");
         return INTERPRET_OK;
       }
     }
