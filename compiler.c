8ada5283710a284c6ec513545016f5a1f83152f8
Valery Zajkov <vzajkov@protonmail.com>
2023-05-04 19:40:17 -0400

    17.4.1 and 17.4.2

--- compiler.c
+++ compiler.c
@@ -78,8 +78,32 @@ static void emitReturn() {
   emitByte(OP_RETURN);
 }
 
+static uint8_t makeConstant(Value value) {
+  int constant = addConstant(currentChunk(), value);
+  if (constant > UINT8_MAX) {
+    error("Too many constants in one chunk.");
+    return 0;
+  }
+
+  return (uint8_t)constant;
+}
+
+static void emitConstant(Value value) {
+  emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
 static void endCompiler() {
   emitReturn();
+}
+
+static void grouping() {
+  /* expression(); */
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+}
+
+static void number() {
+  double value = strtod(parser.previous.start, NULL);
+  emitConstant(value);
 }
 
 bool compile(const char* source, Chunk* chunk) {
