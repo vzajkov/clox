48fc827a44140afc36829e483c53964a8b2e1f32
Valery Zajkov <vzajkov@protonmail.com>
2023-05-06 11:01:26 -0400

    17.4.3

--- compiler.c
+++ compiler.c
@@ -12,6 +12,20 @@ typedef struct {
   bool panicMode;
 } Parser;
 
+typedef enum {
+  PREC_NONE,
+  PREC_ASSIGNMENT,
+  PREC_OR,
+  PREC_AND,
+  PREC_EQUALITY,
+  PREC_COMPARISON,
+  PREC_TERM,
+  PREC_FACTOR,
+  PREC_UNARY,
+  PREC_CALL,
+  PREC_PRIMARY,
+} Precedence;
+
 Parser parser;
 Chunk* compilingChunk;
 
@@ -96,6 +110,14 @@ static void endCompiler() {
   emitReturn();
 }
 
+static void parsePrecedence(Precedence precedence) {
+  //
+}
+
+static void expression() {
+  parsePrecedence(PREC_ASSIGNMENT);
+}
+
 static void grouping() {
   /* expression(); */
   consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
@@ -104,6 +126,17 @@ static void grouping() {
 static void number() {
   double value = strtod(parser.previous.start, NULL);
   emitConstant(value);
+}
+
+static void unary() {
+  TokenType operatorType = parser.previous.type;
+
+  parsePrecedence(PREC_UNARY);
+
+  switch(operatorType) {
+  case TOKEN_MINUS: emitByte(OP_NEGATE); break;
+  default: return;
+  }
 }
 
 bool compile(const char* source, Chunk* chunk) {
